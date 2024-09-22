0941ff52c47abe2d686ebfc1f585fb7b7f0164d7
Donna <skies09@hotmail.com>
2024-09-22 16:27:54 +0100

    update breeds page

--- src/pages/breeds/breedsHome.js
+++ src/pages/breeds/breedsHome.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 
 export default function Breeds() {
 	const [groups, setGroups] = useState([]);
+	const [breeds, setBreeds] = useState([]);
+	const [selectedGroup, setSelectedGroup] = useState(null);
 
 	useEffect(() => {
 		const fetchGroups = async () => {
 			const url = process.env.REACT_APP_NEO_PROJECT_CF_GROUPS_URL;
-			console.log(url, "URL");
 
 			try {
 				const response = await fetch(url, {
@@ -30,32 +31,68 @@ export default function Breeds() {
 		fetchGroups();
 	}, []);
 
+	const fetchBreeds = async (group) => {
+		setSelectedGroup(group);
+		const url = `${process.env.REACT_APP_NEO_PROJECT_CF_BREEDS_URL}${group}`;
+		console.log(url, "URL");
+		try {
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			const breedsData = await response.json();
+			console.log(breedsData, "breed data");
+			setBreeds(breedsData);
+		} catch (error) {
+			console.error("Error fetching breeds:", error.message);
+		}
+	};
+
 	return (
 		<div id="breeds" className="w-screen overflow-hidden h-[80vh] mt-4 ">
-			<div className="flex justify-center items-center font-poppins text-2xl font-bold text-oxfordBlue tracking-wider drop-shadow-md">
+			<div className="flex justify-center items-center font-poppins text-2xl lg:text-3xl font-bold text-oxfordBlue tracking-wider drop-shadow-md">
 				Breeds
 			</div>
-			<div className="group-list">
+			<div className="mt-4">
 				{groups && (
-					<ul>
-						{" "}
+					<ul className="flex justify-center items-center">
 						{groups.map((group, index) => (
-							<li
-								className="m-6 flex justify-center items-center w-1/3"
-								key={index}
-							>
+							<li className="m-2" key={index}>
 								<button
-									className="font-mono text-base text-oxfordBlue bg-sunset p-4 rounded-md"
-									onClick={() => {
-										console.log(group);
-									}}
+									className="font-mono text-base text-oxfordBlue p-4 border rounded-lg hover:bg-sunset"
+									onClick={() => fetchBreeds(group)}
 								>
 									{group}
 								</button>
 							</li>
-						))}{" "}
+						))}
 					</ul>
-				)}{" "}
+				)}
+				<div className="flex justify-center items-center mt-4">
+					<button
+						className="font-mono text-base text-oxfordBlue p-4 border rounded-lg hover:bg-sunset"
+						onClick={() => {
+							console.log("Search all");
+						}}
+					>
+						All Breeds
+					</button>
+				</div>
+				{selectedGroup && breeds.length > 0 && (
+					<div className="mt-4">
+						<h3 className="text-xl font-bold text-center">{`${selectedGroup} Breeds:`}</h3>
+						<ul className="flex flex-wrap justify-center mt-2">
+							{breeds.map((breed, index) => (
+								<li className="m-2" key={breed.id}>
+									<div className="font-mono text-base text-oxfordBlue p-2 border rounded-lg">
+										{breed.breed}{" "}
+									</div>
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
 			</div>
 		</div>
 	);
